f9a034609c769c3c81b81a3bf480ff09d36b53b8
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-03-22 17:44:45 +0530

    git commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { AuthContextProvider } from "./context/AuthContext";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 function App() {
   return (
-    <div className="overflow-hidden">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/account" element={<Account/>} />
+        </Routes>     
+      </AuthContextProvider>
+    </>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBn3SgcHsAsfzJj1_lgt6D0IKTu5kf0iVI",
-  authDomain: "netflix-project34.firebaseapp.com",
-  projectId: "netflix-project34",
-  storageBucket: "netflix-project34.appspot.com",
-  messagingSenderId: "223226314675",
-  appId: "1:223226314675:web:45250ec89b2bed8e4cd5c6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
